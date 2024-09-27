d1287146d868547eeaa539d46f60c94dea14f85c
Sandeep <sandeepsharma999000@gmail.com>
2024-09-27 19:10:56 +0530

    sss

--- compnents/header.tsx
+++ compnents/header.tsx
@@ -8,38 +8,7 @@ const Header = ({ }) => {
   return (
     <header>
        <Navbar fluid rounded>
-      <Navbar.Brand href="/">
-        <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React </span>
-      </Navbar.Brand>
-      <div className="flex md:order-3">
-      <DarkThemeToggle />
-        <Dropdown
-          arrowIcon={false}
-          inline
-          label={
-            <Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded />
-          }
-        >
-          <Dropdown.Header>
-            <span className="block text-sm">Bonnie Green</span>
-            <span className="block truncate text-sm font-medium">name@flowbite.com</span>
-          </Dropdown.Header>
-          <Dropdown.Item>Dashboard</Dropdown.Item>
-          <Dropdown.Item>Settings</Dropdown.Item>
-          <Dropdown.Item>Earnings</Dropdown.Item>
-          <Dropdown.Divider />
-          <Dropdown.Item>Sign out</Dropdown.Item>
-        </Dropdown>
-        <Navbar.Toggle />
-      </div>
-      <Navbar.Collapse>
-        <Navbar.Link href="/" active>
-          Home
-        </Navbar.Link>
-        <Navbar.Link href="form">Service</Navbar.Link>
- 
-      </Navbar.Collapse>
+    
     </Navbar>
 
     </header>
