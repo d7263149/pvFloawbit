52f7d748d905bbe5289433cf969d972ab130899f
Sandeep <sandeepsharma999000@gmail.com>
2024-09-27 19:09:18 +0530

    sss

--- compnents/header.tsx
+++ compnents/header.tsx
@@ -10,7 +10,7 @@ const Header = ({ }) => {
        <Navbar fluid rounded>
       <Navbar.Brand href="/">
         <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React </span>
       </Navbar.Brand>
       <div className="flex md:order-3">
       <DarkThemeToggle />
