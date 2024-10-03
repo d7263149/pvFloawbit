f097b47aa27400b3dd8ca3e0284e0b1c826913f6
Sandeep <sandeepsharma999000@gmail.com>
2024-10-03 18:43:48 +0530

    dddd

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import {signOut, useSession} from 'next-auth/react'
-import Home1 from "./pages/index";
+// import Home1 from "./pages/index";
 export default function Home() {
 // const session = useSession();
 // console.log('session',session);
