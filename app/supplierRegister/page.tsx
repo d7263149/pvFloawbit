2d38e255f82e000ce6c476da3dc9b549cfba7a4c
Sandeep <sandeepsharma999000@gmail.com>
2024-10-16 15:24:15 +0530

    dd

--- app/supplierRegister/page.tsx
+++ app/supplierRegister/page.tsx
@@ -30,28 +30,23 @@ export default function Login() {
   useEffect(() => {
     // Check if the user is already signed in
     if (session?.user) {
-      // Check if all fields are filled in session storage
-      const storedData = sessionStorage.getItem('registerData');
-      if (storedData) {
-        const data: FormData = JSON.parse(storedData);
-        setFormData(data); // Set form data to the stored data
-        // Redirect to registration page if all fields are filled
-        if (data.company && data.contact && data.phone && data.companyEmail && data.description) {
-          router.push('/process');
-        }
-      }
+      // Redirect directly to the registration page if signed in
+      router.push('/process');
     }
   }, [session, router]);
 
   const handleGoogleSignIn = async () => {
-    // Only sign in if the user is not already signed in
-    if (!session?.user) {
-      await signIn('google', { redirect: false });
-    } else {
-      // If already signed in, allow form submission directly
+    // Sign in with Google
+    await signIn('google', { redirect: false }).then(async (response) => {
+      if (response?.error) {
+        console.error('Error signing in with Google:', response.error);
+        return;
+      }
+      
+      // Store form data to session storage if Google sign in is successful
       sessionStorage.setItem('registerData', JSON.stringify(formData));
       router.push('/process'); // Redirect to registration page
-    }
+    });
   };
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
@@ -101,7 +96,7 @@ export default function Login() {
               </div>
             </form>
             <Button onClick={handleGoogleSignIn} color="blue" disabled={isButtonDisabled}>
-              {session?.user ? "Verify" : "Verify"}
+              Verify
             </Button>
           </div>
         </Card>
